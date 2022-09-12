bcf8eb56629f3b1559ab678eb95db0335556cb22
WMs784 <wms784.app@gmail.com>
2022-09-12 06:17:29 +0000

    filter articles by userid

--- lambda/tanaka-k-articles-get-internship.js
+++ lambda/tanaka-k-articles-get-internship.js
@@ -11,27 +11,66 @@ exports.handler = async (event, context) => {
     body: JSON.stringify({ message: "" }),
   };
   
+  // const { userId, start, end, category} = event.queryStringParameters;
+  const userId = event.queryStringParameters?.userId;
+  const category = event.queryStringParameters?.category;
   //TODO: 取得対象のテーブル名をparamに宣言
-  const param = {
-    TableName,
-  };
+  let param;
+  
   
     
   try{
-    // dynamo.scan()で全件取得
-    const articles = (await dynamo.scan(param).promise()).Items;
-    
     if(event.headers.authorization !== "mtiToken"){
       response.statusCode = 401;
       response.body = JSON.stringify({
       message: "認証されていません。HeaderにTokenを指定してください"
       });
     }
-
+    // dynamo.scan()で全件取得
+    if(!userId){
+      param = {
+        TableName,
+        "Limit":100
+      };
+      const articles = (await dynamo.scan(param).promise()).Items;
+      response.statusCode = 201;
+      response.body = JSON.stringify(articles);
+      console.log("all");
+    }
+    else {
+      param = {
+        TableName,
+        "Limit":100,
+        KeyConditionExpression: "userId = :uid",
+        FilterExpression: "category = :ctg",
+        // FilterExpression: {
+        //   "start <= :time",
+        //   end >= :time",
+        //   "category = :cat"
+        // }
+        ExpressionAttributeValues: {
+          ":uid": userId,
+          ":ctg": category
+        }
+      };
+      const filteredArticles = (await dynamo.query(param).promise()).Items;
+      response.statusCode = 201;
+      response.body = JSON.stringify(filteredArticles);
+      console.log("filterd");
+    }
+    
+    // function condition(article) {
+    //   if((this.userId === article.userId) && (this.start === "" || this.start <== article.timestamp) 
+    //   && (this.end === "" || this.end >== article.timestamp) && (this.category === "" || this.category === article.category)){
+    //     return true;
+    //   }
+    //   else return false;
+    // }
     //TODO: レスポンスボディを設定する
-    response.statusCode = 201;
-    response.body = JSON.stringify(articles);
+    
   
+    
+    
   }catch(e){
     response.statusCode = 500;
     response.body = JSON.stringify({
