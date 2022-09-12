292bc3902962b15c81662bc2cc6e2454a4fdb248
WMs784 <wms784.app@gmail.com>
2022-09-12 08:46:49 +0000

    filter with category & time

--- lambda/tanaka-k-articles-get-internship.js
+++ lambda/tanaka-k-articles-get-internship.js
@@ -2,6 +2,61 @@ const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
 const TableName = "Article";
 
+function parameter(userId, start, end, category) {
+  var value = "";
+  
+  let param = {
+    TableName,
+    "Limit": 100,
+    KeyConditionExpression: "userId = :uid",
+    // FilterExpression: value,
+    ExpressionAttributeValues: {
+      ":uid": userId
+    },
+    ExpressionAttributeNames: {
+      "#ts": "timestamp"
+    }
+  };
+  if(category){
+    // param.FilterExpression.push("category = :ctg");
+    value += "category = :ctg";
+    param.ExpressionAttributeValues[":ctg"] = category;
+    param.FilterExpression = "category = :ctg";
+    param.ExpressionAttributeNames["#ts"] = "timestamp";
+    if(start && end){
+      param.KeyConditionExpression += " AND #ts BETWEEN :str AND :end";
+      param.ExpressionAttributeValues[":str"] = parseInt(start);
+      param.ExpressionAttributeValues[":end"] = parseInt(end);
+    }
+    else if(start){
+      param.KeyConditionExpression += " AND #ts >= :str";
+      param.ExpressionAttributeValues[":str"] = parseInt(start);
+    }
+    else if(end){
+      param.KeyConditionExpression += " AND #ts <= :end";
+      param.ExpressionAttributeValues[":end"] = parseInt(end);
+    }
+  }
+  else {
+    param.ExpressionAttributeNames["#ts"] = "timestamp";
+    if(start && end){
+      param.KeyConditionExpression += " AND #ts BETWEEN :str AND :end";
+      param.ExpressionAttributeValues[":str"] = parseInt(start);
+      param.ExpressionAttributeValues[":end"] = parseInt(end);
+    }
+    else if(start){
+      param.KeyConditionExpression += " AND #ts >= :str";
+      param.ExpressionAttributeValues[":str"] = parseInt(start);
+    }
+    else if(end){
+      param.KeyConditionExpression += " AND #ts <= :end";
+      param.ExpressionAttributeValues[":end"] = parseInt(end);
+    }
+  }
+  // param.FilterExpression = value;
+  return param;
+}
+
 exports.handler = async (event, context) => {
   const response = {
     statusCode: 200,
@@ -14,6 +69,8 @@ exports.handler = async (event, context) => {
   // const { userId, start, end, category} = event.queryStringParameters;
   const userId = event.queryStringParameters?.userId;
   const category = event.queryStringParameters?.category;
+  const start = event.queryStringParameters?.start;
+  const end = event.queryStringParameters?.end;
   //TODO: 取得対象のテーブル名をparamに宣言
   let param;
   
@@ -33,28 +90,34 @@ exports.handler = async (event, context) => {
         "Limit":100
       };
       const articles = (await dynamo.scan(param).promise()).Items;
+      articles.sort();
+      articles.reverse();
       response.statusCode = 201;
       response.body = JSON.stringify(articles);
       console.log("all");
     }
-    else {
+    else if (!category && !start && !end) {
       param = {
         TableName,
         "Limit":100,
         KeyConditionExpression: "userId = :uid",
-        FilterExpression: "category = :ctg",
-        // FilterExpression: {
-        //   "start <= :time",
-        //   end >= :time",
-        //   "category = :cat"
-        // }
         ExpressionAttributeValues: {
           ":uid": userId,
-          ":ctg": category
         }
       };
       const filteredArticles = (await dynamo.query(param).promise()).Items;
       response.statusCode = 201;
+      filteredArticles.sort();
+      filteredArticles.reverse();
+      response.body = JSON.stringify(filteredArticles);
+      console.log("filterd");
+    }
+    else {
+      param = parameter(userId, start, end, category);
+      const filteredArticles = (await dynamo.query(param).promise()).Items;
+      response.statusCode = 201;
+      filteredArticles.sort();
+      filteredArticles.reverse();
       response.body = JSON.stringify(filteredArticles);
       console.log("filterd");
     }
@@ -75,6 +138,7 @@ exports.handler = async (event, context) => {
     response.statusCode = 500;
     response.body = JSON.stringify({
       message: "予期せぬエラーが発生しました。",
+      key: param,
       errorDetail: e.toString()
     });
   }
