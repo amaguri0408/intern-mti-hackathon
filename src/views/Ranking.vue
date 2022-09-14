11fd13143bede70aafbba8890b2d1700fbdf70d4
Yuya Kanbetsunawa <kanbe1365@icloud.com>
2022-09-14 08:18:57 +0000

    create ranking view

--- src/views/Ranking.vue
+++ src/views/Ranking.vue
@@ -4,58 +4,31 @@
       <!-- 検索ボックス -->
       <div class="ui segment">
         <form class="ui form">
-          <div class="field">
-            <label>ユーザー名</label>
-            <input type="text" placeholder="Nickname" v-model="nickname" />
-          </div>
-          
-          <div class="field">
-            <label>年齢</label>
-            <div class="inline fields">
-              <div class="field">
-                <input type="text" v-model.number="start" name="agestart" />
-                <label>歳から</label>
-              </div>
-              
-              <div class="field">
-                <input type="text" v-model.number="end" name="ageend" />
-                <label>
-                  歳まで
-                </label>
-              </div>
-            </div>
-          </div>
+          <label>{{myTeamRank}}&emsp;{{myTeamName}}&emsp;{{myTeamPoint}}</label>
+          <br><br>
+          <button @click="tweet()" class="ui orange button">
+            Twitterに投稿
+          </button>
         </form>
       </div>
-      <!-- ユーザー一覧 -->
-      <ul class="ui three column grid">
-        <template v-for="(item, index) in filteredUsers">
-          <li class="column" :key="index">
-            <div class="ui card fluid" >
-              <div class="content">
-                <h2 class="header">
-                  {{ item.nickname }}
-                  <span class="ui green label">{{ item.age }}</span>
-                </h2>
-                <span class="meta">{{ item.userId }} </span>
+      
+      <div class="ui center aligned segment">
+        <label>ランキング</label>
+      
+        <ul class="ui column">
+          <template v-for="(item, index) in rankingList">
+            <li class="column" :key="index">
+              <div class="ui fluid card" >
+                <div class="content">
+                  <label>{{index + 1}}位&emsp;{{item}}</label>
+                </div>
               </div>
-            </div>
-          </li>
-        </template>
-        <!--<templete v-for="(item, index) in users" :key="index">-->
-        <!--  <li class="column" v-for="(item, index) in filteredUsers" :key="index">-->
-        <!--    <div class="ui card fluid">-->
-        <!--      <div class="content">-->
-        <!--        <h2 class="header">-->
-        <!--          {{ item.nickname }}-->
-        <!--          <span class="ui green label">{{ item.age }}</span>-->
-        <!--        </h2>-->
-        <!--        <span class="meta">{{ item.userId }}</span>-->
-        <!--      </div>-->
-        <!--    </div>-->
-        <!--  </li>-->
-        <!--</templete>-->
-      </ul>
+              <br>
+            </li>
+          </template>
+        </ul>
+      </div>
+      
     </div>
   </div>
 </template>
@@ -73,33 +46,19 @@ export default {
   data() {
   // Vue.jsで使う変数はここに記述する
     return {
-      users: [],
-      nickname: "",
-      start: 0,
-      end: 100,
+      myTeamRank: "3位",
+      myTeamName: "筋トレ部A",
+      myTeamPoint: "1000pt",
+      rankingList: ["筋トレ部A", "筋トレ部B", "筋トレ部C"]
     };
   },
   computed: {
-  // 計算した結果を変数として利用したいときはここに記述する
-    filteredUsers() {
-      console.log("!")
-      return this.users.filter(e => 
-        e.nickname?.match(this.nickname) 
-        && e.age >= this.start
-        && e.age <= this.end
-      );
-    }
   },
   methods: {
-  // Vue.jsで使う関数はここで記述する
-    // filteredUsers() {
-    //   console.log("!")
-    //   return this.users.filter(e => 
-    //     e.nickname?.match(this.nickname) 
-    //     && e.age >= this.start
-    //     && e.age <= this.end
-    //   );
-    // }
+    tweet() {
+      
+    }
+  
   },
   created: async function() {
     const token = window.localStorage.getItem('token');
